9d3265a564746dc92f1e388513b278ceadeb9495
dremlin2000 <aibol.toleu@gmail.com>
2019-11-19 15:27:56 +1100

    Progress bars added

--- DynamoDB_RestoreTable2Table.js
+++ DynamoDB_RestoreTable2Table.js
@@ -3,7 +3,8 @@
 //How it works:
 // Please make sure that RCU and WCU on DynamoDB tables are configured correctly so the restoration process will not be slowed down.
 // First restore DynamoDB table from either PITR or On-Demand backup to a new source table
-// Node and AWS SDJ should be installed
+// Node 10 or higher and AWS SDK should be installed
+// Run "myob-auth l"
 // In order to run script please type
 //  node DynamoDB_CopyTable2TableAsync.js <REGION> <SOURCE TABLE NAME> <DESINATION TABLE NAME> -r
 //  where <REGION> is normally ap-southeast-2 (Sydney region)
@@ -14,222 +15,298 @@
 
 const AWS = require('aws-sdk')
 
-const upsertDynamoDbTable = async (db, runParams) => {
+class ProgressBar {
+  constructor() {
+    this.progressBarTitle;
+    this.total;
+    this.current;
+    this.barLength = process.stdout.columns;
+
+    //Attempt to make progress bar responsive
+    process.stdout.on('resize', () => this.update());
+  }
+
+  init(total, progressBarTitle = 'Current progress: ') {
+    this.progressBarTitle = progressBarTitle;
+    this.total = total;
+    this.current = 0;
+    this.update(this.current);
+  }
+
+  update(current) {
+    this.barLength = process.stdout.columns - 12 - this.progressBarTitle.length;
+    if (current) {
+      this.current = current;
+    }
+    const currentProgress = this.current / this.total;
+    this.draw(currentProgress);
+  }
+
+  draw(currentProgress) {
+    const filledBarLength = (currentProgress * this.barLength).toFixed(0);
+    const emptyBarLength = this.barLength - filledBarLength;
+
+    const filledBar = this.getBar(filledBarLength, "\u2588");
+    const emptyBar = this.getBar(emptyBarLength, "\u2591");
+    const percentageProgress = (currentProgress * 100).toFixed(2);
+
+    process.stdout.clearLine();
+    process.stdout.cursorTo(0);
+    process.stdout.write(
+      `${this.progressBarTitle}[${filledBar}${emptyBar}] | ${percentageProgress}%`
+    );
+
+    if (percentageProgress === 100) {
+      process.stdout.write('\n');
+    }
+  }
+
+  getBar(length, char) {
+    let str = "";
+    for (let i = 0; i < length; i++) {
+      str += char;
+    }
+    return str;
+  }
+};
+
+class RestoreDynamoDb {
+  async upsertDynamoDbTable(db, runParams, progressBar) {
     let isDone = false;
     const scanParams = {
-        TableName: runParams.sourceTable
+      TableName: runParams.sourceTable
     };
 
-    logMessage(`---- Start updating records in ${runParams.destinationTable}`);
+    try {
+      this.logMessage(`---- Start upserting records in ${runParams.destinationTable}`);
+      let totalRecNum = 0;
+      let i = 0;
 
-    do {
+      do {
+        i++;
         const data = await db.scan(scanParams).promise();
 
-        await batchWriteDataToTable(db, runParams.destinationTable, data);
+        const batchRecNum = await this.batchWriteDataToTable(db, runParams.destinationTable, data, progressBar, i);
+
+        if (batchRecNum > 0) {
+          this.logMessage(`${batchRecNum} records have been processed`);
+        }
 
         isDone = typeof data.LastEvaluatedKey === "undefined";
 
         // continue scanning if we have more data, because
         // scan can retrieve a maximum of 1MB of data
         if (!isDone) {
-            logMessage("Scanning for more...");
-            scanParams.ExclusiveStartKey = data.LastEvaluatedKey;
+          this.logMessage("Scanning for more...");
+          scanParams.ExclusiveStartKey = data.LastEvaluatedKey;
         }
+
+        totalRecNum += batchRecNum;
+      }
+      while (!isDone)
+
+      this.logMessage(`Total ${totalRecNum} records have been processed`);
+      this.logMessage(`---- Finished upserting records in ${runParams.destinationTable} SUCCESSFULLY!`);
+    } catch (err) {
+      this.logMessage('', null, false);
+      this.logMessage(`---- Finished upserting in ${runParams.destinationTable} UNSUCCESSFULLY!`, err);
     }
-    while (!isDone)
+  }
 
-    logMessage(`---- Finished updating records in ${runParams.destinationTable}`);
-}
-
-// Write all the data to the new table
-const batchWriteDataToTable = async (db, tableName, data) => {
+  // Write all the data to the new table
+  async batchWriteDataToTable(db, tableName, data, progressBar, scanBatchNum) {
     const batchWriteParams = {
-        RequestItems: {
-            [tableName]: []
-        }
+      RequestItems: {
+        [tableName]: []
+      }
     };
 
     let batchRecNum = 0;
-    let successfulUpserts = 0;
+    const len = data.Items.length;
+
+    if (len > 0) {
+      progressBar.init(len, `Progress of scan batch ${scanBatchNum}: `);
+    }
 
     for (const item of data.Items) {
-        batchRecNum++;
-        successfulUpserts++;
+      batchRecNum++;
 
-        batchWriteParams.RequestItems[tableName].push({
-            PutRequest: {
-                Item: item
-            }
-        });
-
-        if (batchRecNum % 25 == 0 || successfulUpserts == data.Items.length) {
-            await db.batchWrite(batchWriteParams,
-                err => {
-                    if (err) {
-                        logMessage(`Unable to upsert records in ${tableName}. Error JSON: ${JSON.stringify(err, null, 2)}`);
-                    }
-                }).promise();
-
-            logMessage(`${successfulUpserts} out of ${data.Items.length} records have been processed`);
-
-            batchRecNum = 0;
-
-            batchWriteParams.RequestItems[tableName] = [];
+      batchWriteParams.RequestItems[tableName].push({
+        PutRequest: {
+          Item: item
         }
+      });
+
+      if (batchRecNum % 25 == 0 || batchRecNum == len) {
+        await db.batchWrite(batchWriteParams).promise();
+        progressBar.update(batchRecNum);
+
+        batchWriteParams.RequestItems[tableName] = [];
+      }
     }
-}
 
-const logMessage = (message) => {
+    return batchRecNum;
+  }
+
+  logMessage(message, err, printTimestamp = true) {
     const dt = new Date();
-    const formattedDateTime =
-        `${(dt.getMonth()+1).toString().padStart(2, '0')}/${
-        dt.getDate().toString().padStart(2, '0')}/${
-        dt.getFullYear().toString().padStart(4, '0')} ${
-        dt.getHours().toString().padStart(2, '0')}:${
-        dt.getMinutes().toString().padStart(2, '0')}:${
-        dt.getSeconds().toString().padStart(2, '0')}.${
-        dt.getMilliseconds().toString().padStart(3, '0')}`;
-    console.log(`${formattedDateTime}: ${message}`);
-}
+    let formattedDateTime = '';
 
-const getKeyForBatchDeleteItem = (tableDescription, dataItem, keepAttributeType = false) => {
+    if (printTimestamp) {
+      formattedDateTime =
+        `${(dt.getMonth()+1).toString().padStart(2, '0')}/${
+          dt.getDate().toString().padStart(2, '0')}/${
+          dt.getFullYear().toString().padStart(4, '0')} ${
+          dt.getHours().toString().padStart(2, '0')}:${
+          dt.getMinutes().toString().padStart(2, '0')}:${
+          dt.getSeconds().toString().padStart(2, '0')}.${
+          dt.getMilliseconds().toString().padStart(3, '0')}: `;
+    }
+
+    if (err) {
+      console.log(`${formattedDateTime}${message} Error:\n`, err);
+    } else {
+      console.log(`${formattedDateTime}${message}`);
+    }
+  }
+
+  getKeyForBatchDeleteItem(tableDescription, dataItem) {
     let result;
     tableDescription.Table.KeySchema.forEach((keyItem) => {
-        result = {
-            ...result,
-            [keyItem.AttributeName]:
-                !keepAttributeType && dataItem[keyItem.AttributeName] !== null &&
-                typeof dataItem[keyItem.AttributeName] === 'object' ?
-                dataItem[keyItem.AttributeName][Object.keys(dataItem[keyItem.AttributeName])[0]] : dataItem[keyItem.AttributeName]
-        }
+      result = {
+        ...result,
+        [keyItem.AttributeName]: dataItem[keyItem.AttributeName]
+      }
     });
 
     return result;
-}
+  }
 
-getItemAttrForBatchDeleteRequest = (tableDescription, dataItem) => {
+  getItemAttrForBatchDeleteRequest(tableDescription, dataItem) {
     return {
-        DeleteRequest: {
-            Key: getKeyForBatchDeleteItem(tableDescription, dataItem)
-        }
+      DeleteRequest: {
+        Key: this.getKeyForBatchDeleteItem(tableDescription, dataItem)
+      }
     };
-}
+  }
 
-const batchDeleteAllRecords = async (db, tableName) => {
+  async batchDeleteAllRecords(db, tableName, progressBar) {
     let isDone = false;
-    let scanParams = {
-        TableName: tableName
-    };
     let hasError = false;
+    let scanParams = {
+      TableName: tableName
+    };
 
-    logMessage(`---- Start deleting from ${tableName}`);
+    try {
+      let totalRecNum = 0;
 
-    // Create the DynamoDB service object
-    var ddb = new AWS.DynamoDB({
+      this.logMessage(`---- Start deleting from ${tableName}`);
+
+      // Create the DynamoDB service object
+      const ddb = new AWS.DynamoDB({
         apiVersion: '2012-08-10'
-    });
+      });
 
-    const tableDescription = await ddb.describeTable({
+      const tableDescription = await ddb.describeTable({
         TableName: tableName
-    }).promise();
+      }).promise();
+      let i = 0;
 
-    do {
-        let successfulDeletes = 0;
+      do {
+        let batchRecNum = 0;
+        const batchWriteParams = {
+          RequestItems: {
+            [tableName]: []
+          }
+        };
 
         const data = await db.scan(scanParams).promise();
+        const len = data.Items.length;
+        i++;
 
-        try {
-            const batchWriteParams = {
-                RequestItems: {
-                    [tableName]: []
-                }
-            };
+        if (len > 0) {
+          progressBar.init(len, `Progress of scan batch ${i}: `);
+        }
 
-            let batchRecNum = 0;
+        for (const item of data.Items) {
+          batchRecNum++;
 
-            for (const item of data.Items) {
-                batchRecNum++;
-                successfulDeletes++;
+          batchWriteParams.RequestItems[tableName].push({
+            ...this.getItemAttrForBatchDeleteRequest(tableDescription, item)
+          });
 
-                batchWriteParams.RequestItems[tableName].push({
-                    ...getItemAttrForBatchDeleteRequest(tableDescription, item)
-                });
+          if (batchRecNum % 25 == 0 || batchRecNum == len) {
+            await db.batchWrite(batchWriteParams).promise();
 
-                if (batchRecNum % 25 == 0 || successfulDeletes == data.Items.length) {
-                    await db.batchWrite(batchWriteParams,
-                        err => {
-                            if (err) {
-                                logMessage(`Unable to delete records from ${tableName}. Error JSON: ${JSON.stringify(err, null, 2)}`);
-                            }
-                        }).promise();
+            progressBar.update(batchRecNum);
 
-                    logMessage(`${successfulDeletes} out of ${data.Items.length} records have been deleted`);
-
-                    batchRecNum = 0;
-
-                    batchWriteParams.RequestItems[tableName] = [];
-                }
-            }
-        } catch (e) {
-            // If something fails, log it
-            logMessage('error', e);
-            hasError = true;
-            break;
+            batchWriteParams.RequestItems[tableName] = [];
+          }
         }
 
         isDone = typeof data.LastEvaluatedKey === "undefined";
 
+        if (batchRecNum > 0) {
+          this.logMessage(`${batchRecNum} records have been deleted`);
+        }
+
         // continue scanning if we have more date, because
         // scan can retrieve a maximum of 1MB of data
         if (!isDone) {
-            logMessage("Scanning more records...");
-            scanParams.ExclusiveStartKey = data.LastEvaluatedKey;
+          this.logMessage("Scanning more records...");
+          scanParams.ExclusiveStartKey = data.LastEvaluatedKey;
         }
-    }
-    while (!isDone)
+        totalRecNum += batchRecNum;
+      }
+      while (!isDone)
 
-    logMessage(`---- Finished deleting from ${tableName}`);
+      //this.logMessage('', null, false);
+      this.logMessage(`Total ${totalRecNum} records have been deleted`);
+      this.logMessage(`---- Finished deleting from ${tableName} SUCCESSFULLY`);
+    } catch (err) {
+      hasError = true;
+      this.logMessage('', null, false);
+      this.logMessage(`---- Finished deleting from ${tableName} UNSUCCESSFULLY!`, err);
+    }
 
     return hasError;
-}
+  }
 
-const runTask = async (region, runParams) => {
+  async restoreTable(params) {
+    AWS.config.update({
+      region: params.region
+    });
 
+    const db = new AWS.DynamoDB.DocumentClient({
+      maxRetries: 13,
+      retryDelayOptions: {
+        base: 200
+      }
+    });
+
+    const progressBar = new ProgressBar();
+
+    if (params.deleteAllRecords && await this.batchDeleteAllRecords(db, params.destinationTable, progressBar)) {
+      return;
+    }
+
+    await this.upsertDynamoDbTable(db, params, progressBar);
+
+    this.logMessage('--------- Job is done!');
+  }
 }
 
 // Run the script
 (async () => {
-    const args = process.argv.slice(2);
+  const args = process.argv.slice(2);
 
-    const runParams = {
-        sourceTable: args[1],
-        destinationTable: args[2],
-        deleteAllRecords: false
-    };
-
-    runParams.deleteAllRecords = args.indexOf('-r') > 0;
-
-    AWS.config.update({
-        region: args[0]
-    });
-
-    const db = new AWS.DynamoDB.DocumentClient({
-        maxRetries: 13,
-        retryDelayOptions: {
-            base: 200
-        }
-    });
-
-    if (runParams.deleteAllRecords) {
-        const hasError = await batchDeleteAllRecords(db, runParams.destinationTable);
-        if (hasError) {
-            return;
-        }
-    }
-
-    await upsertDynamoDbTable(db, runParams);
-
-    logMessage('--------- Job is done!');
-
-    await runTask(args[0], runParams);
+  const runParams = {
+    region: args[0],
+    sourceTable: args[1],
+    destinationTable: args[2],
+    deleteAllRecords: args.indexOf('-r') > 0
+  };
+  
+  const restoreDynamoDb = new RestoreDynamoDb();
+  await restoreDynamoDb.restoreTable(runParams);
 })()
